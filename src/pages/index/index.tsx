adc444606df4da7b6864cba8090dd8f6fde6b0ac
Fled8617 <35049517+Fled8617@users.noreply.github.com>
2020-04-16 13:40:10 +0800

    test

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import { View } from "@tarojs/components";
+import { observer, inject } from "mobx-react";
+import { AtModal } from "taro-ui";
+import "./index.scss";
+
+type PageStateProps = {
+  store: {
+    counterStore: {
+      counter: number;
+      increment: Function;
+      decrement: Function;
+      incrementAsync: Function;
+    };
+  };
+};
+
+interface Index {
+  props: PageStateProps;
+}
+
+@inject("store")
+@observer
+class Index extends Component {
+  componentWillMount() {}
+
+  componentDidMount() {}
+
+  componentWillUnmount() {}
+
+  componentDidShow() {}
+
+  componentDidHide() {}
+
+  increment = () => {
+    const { counterStore } = this.props.store;
+    counterStore.increment();
+  };
+
+  decrement = () => {
+    const { counterStore } = this.props.store;
+    counterStore.decrement();
+  };
+
+  incrementAsync = () => {
+    const { counterStore } = this.props.store;
+    counterStore.incrementAsync();
+  };
+
+  render() {
+    const {
+    } = this.props.store;
+    return (
+      <View className="index">
+        <AtModal isOpened>modal弹出后后面依旧可以滚动</AtModal>
+        {new Array(100).fill(1).map((_, i) => (
+          <View key={`g${i}`}>还是可以滚动{i}</View>
+        ))}
+      </View>
+    );
+  }
+}
+
+export default Index;
